569269d2c8371babb543aaa41ba421fb191853f2
SacRich <richard.salzmann42@gmail.com>
2023-07-09 19:14:39 +0200

    main loop and logic, lots of comments

--- Amaze_ConsoleApp/Program.cs
+++ Amaze_ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
-﻿using ExtensionMethods;
-using HightechICT.Amazeing.Client.Rest;
+﻿using HightechICT.Amazeing.Client.Rest;
 using System.Globalization;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -21,8 +20,7 @@ namespace MyApp
             //Global variables
             string authToken = "HTI Thanks You [3KE]";
             string playerName = "RichardS";
-            //The maze we choose to enter, need to make this choice dynamic
-            string currentlyChosenMaze = "Example Maze";
+            MazeInfo currentMazeInfo = new MazeInfo();
 
             client.DefaultRequestHeaders.Add("Authorization", $"{authToken}");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -41,68 +39,10 @@ namespace MyApp
 
                 Console.ReadKey();
 
-                //Let's see if we are in a maze already
+                //All the mazes with info
+                ICollection<MazeInfo> allMazeInfo = await amazeingClient.AllMazes();
 
-                //If not, then enter one (TODO: Change it to search one from allMazes - also flag here what we have already entered)
-                if (!playerInfo.IsInMaze)
-                {
-                    try
-                    {
-                        await amazeingClient.EnterMaze($"{currentlyChosenMaze}");
-                    }
-                    catch (ApiException mazeEnterException)
-                    {
-                        Console.WriteLine(mazeEnterException.Message);
-                        return;
-                    }
-                }
-                //If yes, we traverse it
-
-                Console.WriteLine($"Current Maze: {playerInfo.Maze}");
-                Console.ReadKey();
-
-                //Get the possible actions first
-
-                //This is the info about a single maze we need this only where we decide which maze to enter
-                MazeInfo currentMazeInfo = new MazeInfo();
-
-                PossibleActionsAndCurrentScore possibleActionsAndCurrentScore = new PossibleActionsAndCurrentScore();
-
-                possibleActionsAndCurrentScore = await amazeingClient.PossibleActions();
-
-                string readableInfo;
-
-                if(possibleActionsAndCurrentScore.CurrentScoreInBag == 0)
-                {
-                    readableInfo = "No score yet in bag!";
-                }
-                else readableInfo = possibleActionsAndCurrentScore.CurrentScoreInBag.ToString();
-
-                Console.WriteLine(readableInfo);
-
-                //Make a map somehow
-                //This will be a coordinate system - need to assign coordinate beside (together?) with the int ID
-                //Encode coordinate into id
-                //Decode coordinate from id 
-
-                //Tag current tile
-
-                await amazeingClient.TagTile();
-
-                //Decide where we can go and where we should go - compare map? do we need map if we can just look ahead
-                //Need to save those tiles, that we have visited but need to visit again bc didn't visit one of their neighbours
-
-                ICollection<MoveAction> possibleMoveActions = possibleActionsAndCurrentScore.PossibleMoveActions;
-
-                foreach (var action in possibleMoveActions)
-                {
-                    Console.WriteLine(action.Direction.ToString());
-
-                    //amazeingClient.Move(action.Direction);
-                }
-
-                Console.ReadKey();
-
+                TraverseAMaze(amazeingClient, playerInfo, allMazeInfo);
 
 
             }
@@ -143,85 +83,123 @@ namespace MyApp
             }
         }
 
-        private static HttpClient client = new HttpClient()
+        //Calls choose next maze, enters it, and get's 
+        private static async void TraverseAMaze(AmazeingClient amazeingClient,PlayerInfo playerInfo ,ICollection<MazeInfo> allMazeInfo)
         {
-            BaseAddress = new Uri("https://maze.hightechict.nl"),
-        };
+            //If we are in a maze somehow already it's testing phase and we can ignore this
 
-    }
-}
+            //If not, then enter one - this just chooses one and returns the list without the current maze
+            if (!playerInfo.IsInMaze)
+            {
+                allMazeInfo = await ChooseNextMaze(amazeingClient, allMazeInfo);
+            }
+            //Get the possible actions first
 
-namespace ExtensionMethods
-{
-    public static class ExtensionMethods
-    {
-        public static Task TagTile(this AmazeingClient amazeingClient)
-        {
-            return TagTile(CancellationToken.None, amazeingClient);
+            PossibleActionsAndCurrentScore possibleActionsAndCurrentScore = new PossibleActionsAndCurrentScore();
+
+            possibleActionsAndCurrentScore = await amazeingClient.PossibleActions();
+
+            //Tile - how to map this
+
+            //I want a list - where we put coordinates
+
+            //I want to make a method for creating the new coordinates
+
+            //This reduces the need for API calls right?
+
+            //IF moveaction up - add y+1
+            //IF moveaction right - add x+1
+            //IF moveaction down - add y-1
+            //IF moveaction left - add x-1
+
+            //Then we compare 
+
+            //If we have coordinates we could save exit points and collecting points globally, so we can always ask them
+            //If we have the current coordinates and where do we want to go we at least can make a direction suggestion
+            //So we go in the right direction and don't wast time
+            //This suggestion could be a param (optional?) to the moves so it prefers those directions that are the closest
+
+
+
+            //MoveActions 
+
+            ICollection<MoveAction> possibleMoveActions = possibleActionsAndCurrentScore.PossibleMoveActions;
+
+            //First decide what the goal is, there are 3 stages
+            // - Look for scores and collect them by stepping on it. We only need to look further if we don't have the max score
+            // - Look for a score collection point
+            // - Look for exit point
+
+
+
+
+            foreach (var adjacentTile in possibleMoveActions)
+            {
+                //This doesnt need to be a foreach 
+
+                //Option1: - we can only go where we came from
+                //We have 1 possibleMoveAction, we go that way which is probably visited (except start)
+
+                //Option2: - we can go where we came from and one more, so
+                //We have 2 possibleMoveAction
+                // - we go one way - which is not visited
+                // - the other is where we coming from
+
+                //Option3:
+                //We have 3 possibleMoveAction
+                // - one is where we came from
+                // - Need to choose between the two
+                //IF we have visited one and not the other, then go that way
+                //ELSE IF we have visited both 
+
+
+                //Option4: this is a cross section we can go anywhere
+                //We have 4 possibleMoveAction
+                // - one where we came from
+                // - three others
+
+                //If we haven't visited this tile yet
+                if (!adjacentTile.HasBeenVisited)
+                {
+                    await amazeingClient.Move(adjacentTile.Direction);
+                    possibleActionsAndCurrentScore = await amazeingClient.PossibleActions();
+
+                }
+
+            }
+
+            await amazeingClient.ExitMaze();
+
+
+            //If we have more mazes in our list, we enter the next one
+            if(allMazeInfo.Count > 0)
+            {
+                var updatedPlayerInfo = await amazeingClient.GetPlayerInfo();
+                TraverseAMaze(amazeingClient, updatedPlayerInfo, allMazeInfo);
+            }
         }
 
-        public static async Task TagTile(CancellationToken cancellationToken, AmazeingClient amazeingClient)
+        public static async Task<ICollection<MazeInfo>> ChooseNextMaze(AmazeingClient amazeingClient, ICollection<MazeInfo> allMazeInfo)
         {
-            client.DefaultRequestHeaders.Add("Authorization", "HTI Thanks You [3KE]");
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var tagValue = 10;
-            string BaseAddress = "https://maze.hightechict.nl";
-            StringBuilder urlBuilder_ = new StringBuilder();
-            urlBuilder_.Append((BaseAddress != null) ? BaseAddress.TrimEnd('/') : "").Append("/api/maze/tag?");
-            urlBuilder_.Append("tagValue" + "=").Append(tagValue.ToString());
-            //urlBuilder_.Length--;
-            HttpClient client_ = client;
             try
             {
-                using HttpRequestMessage request_ = new HttpRequestMessage();
-                request_.Content = new StringContent(string.Empty, Encoding.UTF8, "application/json");
-                request_.Method = new HttpMethod("POST");
-                request_.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
-                string url_ = urlBuilder_.ToString();
-                request_.RequestUri = new Uri(url_, UriKind.RelativeOrAbsolute);
-                HttpResponseMessage response_ = await client_.SendAsync(request_, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(continueOnCapturedContext: false);
-                try
+                //Choose one to enter from the allMazeInfo list
+                foreach (var maze in allMazeInfo)
                 {
-                    Dictionary<string, IEnumerable<string>> headers_ = response_.Headers.ToDictionary<KeyValuePair<string, IEnumerable<string>>, string, IEnumerable<string>>((KeyValuePair<string, IEnumerable<string>> h_) => h_.Key, (KeyValuePair<string, IEnumerable<string>> h_) => h_.Value);
-                    if (response_.Content != null && response_.Content.Headers != null)
-                    {
-                        foreach (KeyValuePair<string, IEnumerable<string>> item_ in response_.Content.Headers)
-                        {
-                            headers_[item_.Key] = item_.Value;
-                        }
-                    }
+                    await amazeingClient.EnterMaze($"{maze.Name}");
 
-                    string status_ = ((int)response_.StatusCode).ToString();
-                    if (status_ == "200")
-                    {
-                        //return (await ReadObjectResponse<PossibleActionsAndCurrentScore>(response_, headers_).ConfigureAwait(continueOnCapturedContext: false)).Object;
-                        return;
-                    }
-
-                    if (status_ == "412")
-                    {
-                        string text = ((response_.Content != null) ? (await response_.Content.ReadAsStringAsync().ConfigureAwait(continueOnCapturedContext: false)) : string.Empty);
-                        string responseText_ = text;
-                        throw new ApiException("You haven't entered a maze yet. Use the enter action to get this party started.", (int)response_.StatusCode, responseText_, headers_, null);
-                    }
-
-                    if (status_ != "200" && status_ != "204")
-                    {
-                        string text2 = ((response_.Content != null) ? (await response_.Content.ReadAsStringAsync().ConfigureAwait(continueOnCapturedContext: false)) : null);
-                        string responseData_ = text2;
-                        throw new ApiException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
-                    }
-
-                    return;
+                    //If we succeed, we need to remove it from the list 
+                    allMazeInfo.Remove(maze);
+                    //Return the shortened list
+                    return allMazeInfo;
                 }
-                finally
-                {
-                    response_?.Dispose();
-                }
+                //No more mazes:
+                return allMazeInfo;
             }
-            finally
+            catch (ApiException mazeEnterException)
             {
-
+                Console.WriteLine(mazeEnterException.Message);
+                return allMazeInfo;
             }
         }
 
